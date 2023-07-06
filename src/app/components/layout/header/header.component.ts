4ed853b9ba147303c13ed44d200da867b72e261d
Javier Gonzalez-Diaz Madroñero <javier.gonzalez.diaz@external.wolterskluwer.com>
2023-07-06 19:08:07 +0200

    limpieza zzzzzz

--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -4,7 +4,7 @@ import { User } from '../../models/user.model';
 @Component({
   selector: 'app-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.css']
+  styleUrls: ['./header.component.scss']
 })
 export class HeaderComponent implements OnInit {
   currentUser: User
