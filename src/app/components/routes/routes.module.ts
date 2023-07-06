d324d912c1f600af5a2f9c930c434d8333a7208c
Javier Gonzalez-Diaz Madroñero <javier.gonzalez.diaz@external.wolterskluwer.com>
2023-07-06 18:56:26 +0200

    mas limpieza

--- src/app/components/routes/routes.module.ts
+++ src/app/components/routes/routes.module.ts
@@ -3,23 +3,23 @@ import { CommonModule } from '@angular/common';
 import { HomeComponent } from './home/home.component';
 import { InicioComponent } from './inicio/inicio.component';
 import { LoadingComponent } from './loading/loading.component';
-
-
+import { LayoutModule } from "../layout/layout.module";
+import { HeaderComponent } from '../layout/header/header.component';
 
 @NgModule({
-  declarations: [
-    HomeComponent,
-    InicioComponent,
-    LoadingComponent
-  ],
-  exports: [
-    HomeComponent,
-    InicioComponent,
-    LoadingComponent
-  ],
-  imports: [
-    CommonModule
-  ]
-
+    declarations: [
+        HomeComponent,
+        InicioComponent,
+        LoadingComponent
+    ],
+    exports: [
+        HomeComponent,
+        InicioComponent,
+        LoadingComponent
+    ],
+    imports: [
+        CommonModule,
+        LayoutModule
+    ]
 })
 export class RoutesModule { }
