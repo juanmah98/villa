0bd7477253afdf13c1d46fbdc71f59a9e2cfa0f2
Javier Gonzalez-Diaz Madroñero <javier.gonzalez.diaz@external.wolterskluwer.com>
2023-07-06 18:51:10 +0200

    Seguimos limpiando

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,10 +2,11 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { HomeComponent } from './components/routes/home/home.component';
 import { InicioComponent } from './components/routes/inicio/inicio.component';
+import { LoadingComponent } from './components/routes/loading/loading.component';
 
 const routes: Routes = [
   {path: '', component: InicioComponent},
-  {path: 'loading', component: HomeComponent},
+  {path: 'loading', component: LoadingComponent},
   {path: 'home', component: HomeComponent},
 
 ];
--- src/app/components/layout/header/header.component.ts
+++ src/app/components/layout/header/header.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { User } from '../../models/user.model';
 
 @Component({
   selector: 'app-header',
@@ -6,6 +7,8 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./header.component.css']
 })
 export class HeaderComponent implements OnInit {
+  currentUser: User
+
   constructor() { }
 
   ngOnInit(): void {
