117a8bcc090c80dee6ea37993651d7af2df4e6a6
Javier Gonzalez-Diaz Madroñero <javier.gonzalez.diaz@external.wolterskluwer.com>
2023-07-07 14:51:35 +0200

    moviendo carpetas de nuevo bro sorry

--- src/app/components/layout/layout.module.ts
+++ src/app/components/layout/layout.module.ts
@@ -1,7 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-import { MedalsComponent } from './medals/medals.component';
+import { MedalsComponent } from './header/medals/medals.component';
 
 @NgModule({
   declarations: [
