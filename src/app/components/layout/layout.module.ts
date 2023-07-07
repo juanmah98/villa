4ebec477e7d1e0c4552d6ffee5089029c25bfdae
Javier Gonzalez-Diaz Madroñero <javier.gonzalez.diaz@external.wolterskluwer.com>
2023-07-07 13:11:56 +0200

    Creamos el componente de medallas

--- src/app/components/layout/layout.module.ts
+++ src/app/components/layout/layout.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
+import { MedalsComponent } from './medals/medals/medals.component';
 
 
 
 @NgModule({
   declarations: [
-    HeaderComponent
+    HeaderComponent,
+    MedalsComponent
   ],
   exports: [
     HeaderComponent
---
+++ src/app/components/layout/medals/medals/medals.component.ts
@@ -0,0 +1,10 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-medals',
+  templateUrl: './medals.component.html',
+  styleUrls: ['./medals.component.scss']
+})
+export class MedalsComponent {
+
+}
