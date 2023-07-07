704490d55b810e87c1a1d2e57a7b7d306ac556fe
Javier Gonzalez-Diaz Madroñero <javier.gonzalez.diaz@external.wolterskluwer.com>
2023-07-07 20:00:15 +0200

    asd

--- src/app/components/routes/home/home.component.ts
+++ src/app/components/routes/home/home.component.ts
@@ -5,7 +5,7 @@ import { User } from '../../models/user.model';
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.css']
+  styleUrls: ['./home.component.scss']
 })
 export class HomeComponent implements OnInit {
   currentUser: User;
