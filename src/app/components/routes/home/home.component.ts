26c7e5cc370e72a9d9cbe7766da43616cbdde1fa
Javier Gonzalez-Diaz Madroñero <javier.gonzalez.diaz@external.wolterskluwer.com>
2023-07-07 21:13:06 +0200

    ajustada la imagen cuando la pantalla se hace pequeña, cambio de bootstrap a scss en el home

--- src/app/components/routes/home/home.component.ts
+++ src/app/components/routes/home/home.component.ts
@@ -9,11 +9,14 @@ import { User } from '../../models/user.model';
 })
 export class HomeComponent implements OnInit {
   currentUser: User;
+  description: string;
 
   constructor() { }
 
   ngOnInit(): void {
     const storedUserString = localStorage.getItem("user");
     this.currentUser = JSON.parse(storedUserString);
+
+    this.description = "Sexta edicion de la Villa. Donde por primera vez repetimos destino, fue la casa seleccionada para 2021, la segunda de España. En esta ocasión confiamos despertarnos de nuevo con churros para todos y resaca para nadie."
   }
 }
