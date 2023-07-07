aa9855a92c72ad383e65ce6e2c2a04892e1cb08d
Javier Gonzalez-Diaz Madroñero <javier.gonzalez.diaz@external.wolterskluwer.com>
2023-07-07 13:14:05 +0200

    lio de carpetas arreglado

--- src/app/components/layout/layout.module.ts
+++ src/app/components/layout/layout.module.ts
@@ -1,9 +1,7 @@
 import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { HeaderComponent } from './header/header.component';
-import { MedalsComponent } from './medals/medals/medals.component';
-
-
+import { MedalsComponent } from './medals/medals.component';
 
 @NgModule({
   declarations: [
