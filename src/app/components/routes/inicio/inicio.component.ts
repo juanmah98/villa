19192265f8c06110bf1d546b1e06081d7d344576
JuanmaH198 <juanmah1998@gmail.com>
2023-07-08 18:29:50 +0200

    inicio google resuelto y audio en loading

--- src/app/components/routes/inicio/inicio.component.ts
+++ src/app/components/routes/inicio/inicio.component.ts
@@ -11,13 +11,13 @@ export class InicioComponent implements OnInit {
 
   constructor() { }
 
-  ngOnInit(): void {
-    google.accounts.id.initialize({
+  async ngOnInit(): Promise<void> {
+   await google.accounts.id.initialize({
       client_id: '112109968528-8s98plf90uargeq2t4imemec1ilap2j9.apps.googleusercontent.com',
       callback: this.handleCredentialResponse
     });
 
-    google.accounts.id.renderButton(
+   await google.accounts.id.renderButton(
       document.getElementById("buttonDiv"),
       { theme: "outline", size: "large" }
     );
