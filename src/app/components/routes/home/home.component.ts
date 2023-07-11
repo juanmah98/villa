ca436c0e371b4d5d53742e0316fe50887a3df0b7
Javier Gonzalez-Diaz Madroñero <javier.gonzalez.diaz@external.wolterskluwer.com>
2023-07-11 21:21:05 +0200

    Scroll automatico con pantalla de carga en home

--- src/app/components/routes/home/home.component.ts
+++ src/app/components/routes/home/home.component.ts
@@ -1,18 +1,19 @@
-import { Component, OnInit, Renderer2 } from '@angular/core';
-import { DatabaseService } from 'src/app/services/database.service';
+import { AfterViewInit, Component, OnDestroy, OnInit } from '@angular/core';
 import { User } from '../../models/user.model';
+import { Subject, fromEvent, takeUntil } from 'rxjs';
 
 @Component({
   selector: 'app-home',
   templateUrl: './home.component.html',
   styleUrls: ['./home.component.scss']
 })
-export class HomeComponent implements OnInit {
+export class HomeComponent implements OnInit, AfterViewInit {
   currentUser: User;
   description: string;
   encuesta: string;
   playlist: string;
   tricuount: string;
+  blackOverlayVisible: boolean = true
 
   constructor() { }
 
@@ -27,7 +28,16 @@ export class HomeComponent implements OnInit {
     this.tricuount = "Apunta y consulta cuanto dinero debes y te deben de manera fácil y rápida"
     this.playlist = "¿Quieres escuchar una lista que ha sonado y no te acuerdas de cual es? Seguramente este aquí."
     this.encuesta = "¿Dónde cenamos? ¿Qué día nos disfrazamos? ¿Cuál es la tematica de este año? ¿Alguna sugerencia?"
+  }
 
+  ngAfterViewInit() {
+    const scrollPosition = 350;
+
+    window.scrollTo({
+      top: scrollPosition
+    });
+
+    this.blackOverlayVisible = false
   }
 
   onLoguut(): void {
