3d18acefb4f15a5f4471a636f3d7bceb8d9aa14e
Javier Gonzalez-Diaz Madroñero <javier.gonzalez.diaz@external.wolterskluwer.com>
2023-07-21 17:25:24 +0200

    añadida ubicacion de la cena

--- src/app/components/layout/popup-info/popup-info.component.ts
+++ src/app/components/layout/popup-info/popup-info.component.ts
@@ -7,10 +7,17 @@ import { PopupService } from 'src/app/services/popup.service';
   styleUrls: ['./popup-info.component.scss']
 })
 export class PopupInfoComponent {
+
+  enlaceUbicacionCena = "https://goo.gl/maps/irUZfyqXY71C5nHEA"
+
   constructor(private popupService: PopupService) { }
   description = "Sexta edición de la Villa. donde por primera vez repetimos destino, fue la casa selecionada para 2021, la segunda de España. En esta ocasión confiamos despertanos de nuevo con churros para todos y resaca para nadie. Ubicada en Finestrat, cerca de Benidormcon una capacidad de 20 camas aseguradas, zona de barbacoa al lado de la piscina, aislada de los vecinos, además de varias neveras industriales y cocina grande. La casa esta a 10 minutos en coche del supermercado más cercano, lo que nos permite reponer cualquier cosa en cuanto falte, además de tener fácil acceso y espacio para aparcar."
 
   onBackgroundTouched() {
     this.popupService.actualizarMostrar(false)
+  }
+
+  onSecundaryAdressTouched() {
+    window.open(this.enlaceUbicacionCena, '_blank');
   }
 }
