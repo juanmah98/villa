e4badd95df0b2db9a4b7550b6bc8e85df978df5e
Javier Gonzalez-Diaz Madroñero <javier.gonzalez.diaz@external.wolterskluwer.com>
2023-07-21 14:26:33 +0200

    seguimos añadiendo svgs

--- src/app/components/routes/home/medals-collection/medals-collection.component.ts
+++ src/app/components/routes/home/medals-collection/medals-collection.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { DomSanitizer, SafeResourceUrl } from '@angular/platform-browser';
 import { User } from 'src/app/components/models/user.model';
 import { MedalsPopupService } from 'src/app/services/medals-popup.service';
 
@@ -15,9 +16,24 @@ export class MedalsCollectionComponent {
     cobre: []  // Medallas de cobre
   }];
 
+  goldMedalImg: SafeResourceUrl;
+  silverMedalImg: SafeResourceUrl;
+  bronzeMedalImg: SafeResourceUrl;
+  copperMedalImg: SafeResourceUrl;
+
   private currentUser: User
 
-  constructor(public medalsPopupService: MedalsPopupService) { }
+  constructor(public medalsPopupService: MedalsPopupService, private sanitizer: DomSanitizer) {
+    const goldUrl = "../../../../../assets/svg/GoldMedalIcon.svg";
+    this.goldMedalImg = this.sanitizer.bypassSecurityTrustResourceUrl(goldUrl);
+    const silverUrl = "../../../../../assets/svg/SilverMedalIcon.svg";
+    this.silverMedalImg = this.sanitizer.bypassSecurityTrustResourceUrl(silverUrl);
+    const bronzeUrl = "../../../../../assets/svg/BronzeMedalIcon.svg";
+    this.bronzeMedalImg = this.sanitizer.bypassSecurityTrustResourceUrl(bronzeUrl);
+    const copperUrl = "../../../../../assets/svg/CopperMedalIcon.svg";
+    this.copperMedalImg = this.sanitizer.bypassSecurityTrustResourceUrl(copperUrl);
+  }
+
 
   ngOnInit(): void {
     const storedUserString = localStorage.getItem("user");
