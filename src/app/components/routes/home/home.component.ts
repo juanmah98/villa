08c923af755a1554c893b95d63e5315c2a48222c
Javier Gonzalez-Diaz Madroñero <javier.gonzalez.diaz@external.wolterskluwer.com>
2023-07-22 22:41:14 +0200

    asd

--- src/app/components/routes/home/home.component.ts
+++ src/app/components/routes/home/home.component.ts
@@ -41,7 +41,7 @@ export class HomeComponent implements OnInit, AfterViewInit {
     //Este quizas lo podemos pasar a la base de datos, asi tenemos capacidad de modificar estos valores sin tener que volver a montar la app, ya que en cada inicio accederia a los valores de firebase
     this.description = "Sexta edición de la Villa. Donde por primera vez repetimos destino, fue la casa seleccionada para 2021, la segunda de España. En esta ocasión confiamos despertarnos de nuevo con churros para todos y resaca para nadie."
 
-    this.tricuount = "Apunta y consulta cuanto dinero debes y te deben de manera fácil y rápida"
+    this.tricuount = "Apunta y consulta cuánto dinero debes y te deben de manera fácil y rápida"
     this.playlist = "¿Quieres escuchar una lista que ha sonado y no te acuerdas de cuál es? Seguramente este aquí."
     this.encuesta = "¿Dónde cenamos? ¿Qué día nos disfrazamos? ¿Cuál es la temática de este año? ¿Alguna sugerencia?"
 
