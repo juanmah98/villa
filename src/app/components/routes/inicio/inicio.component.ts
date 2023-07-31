d8bffbf870feae433d0916a5ea2d978d94a93d39
Javier Gonzalez-Diaz Madroñero <javier.gonzalez.diaz@external.wolterskluwer.com>
2023-07-31 14:29:55 +0200

    prueba login google

--- src/app/components/routes/inicio/inicio.component.ts
+++ src/app/components/routes/inicio/inicio.component.ts
@@ -12,17 +12,19 @@ export class InicioComponent implements OnInit {
   constructor() {}
 
   async ngOnInit(): Promise<void> {
-   await google.accounts.id.initialize({
-      client_id: '112109968528-8s98plf90uargeq2t4imemec1ilap2j9.apps.googleusercontent.com',
-      callback: this.handleCredentialResponse
-    });
+   setTimeout(() => {
+     google.accounts.id.initialize({
+        client_id: '112109968528-8s98plf90uargeq2t4imemec1ilap2j9.apps.googleusercontent.com',
+        callback: this.handleCredentialResponse
+      });
 
-    await google.accounts.id.prompt();
+      google.accounts.id.prompt();
 
-    await google.accounts.id.renderButton(
-      document.getElementById("buttonDiv"),
-      { theme: "outline", size: "large" }
-    );
+      google.accounts.id.renderButton(
+        document.getElementById("buttonDiv"),
+        { theme: "outline", size: "large" }
+      );
+   }, 1000)
   }
 
   handleCredentialResponse(response: any) {
