426583250dcfb8aa188437604bde1f7099af1c46
Javier Gonzalez-Diaz Madroñero <javier.gonzalez.diaz@external.wolterskluwer.com>
2023-08-14 20:14:39 +0200

    Añadida ubicacion de la reserva dia 25

--- src/app/components/layout/popup-info/popup-info.component.ts
+++ src/app/components/layout/popup-info/popup-info.component.ts
@@ -8,7 +8,7 @@ import { PopupService } from 'src/app/services/popup.service';
 })
 export class PopupInfoComponent {
 
-  enlaceUbicacionCena = "https://goo.gl/maps/irUZfyqXY71C5nHEA"
+  enlaceUbicacionCena = "https://www.thefork.es/restaurante/el-rey-r728506"
 
   constructor(private popupService: PopupService) { }
   description = "Sexta edición de la Villa. Donde por primera vez repetimos destino, fue la casa seleccionada para 2021, la segunda de España. En esta ocasión confiamos despertanos de nuevo con churros para todos y resaca para nadie. Ubicada en Finestrat, cerca de Benidorm una capacidad de 20 camas aseguradas, zona de barbacoa al lado de la piscina, aislada de los vecinos, además de varias neveras industriales y cocina grande. La casa está a 10 minutos en coche del supermercado más cercano, lo que nos permite reponer cualquier cosa en cuanto falte, además de tener fácil acceso y espacio para aparcar."
